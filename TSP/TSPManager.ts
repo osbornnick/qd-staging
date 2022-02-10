edb0a88f685c66ebc1bab2623822e05e0fa2f3a5
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-02-10 10:05:20 -0500

    tsp manager is behaving

---
+++ TSP/TSPManager.ts
@@ -0,0 +1,97 @@
+import BehaviorController from "../controller/BehaviorController.js";
+import TaskController from "../controller/TaskController.js";
+import TSPBehaviorController from "./TSPBehaviorController.js";
+import TSPTaskController from "./TSPTaskController.js";
+import Manager from "../manager/Manager.js";
+import Solution from "../interfaces/Solution.js";
+
+export default class TSPManager implements Manager {
+    userID: String;
+    behaviorVisible: boolean;
+    bestSolution: any;
+    previousSolution: Solution;
+    currentSolution: Solution = [[]];
+    bestScore: number = 0;
+    taskController: TaskController;
+    behaviorController: BehaviorController;
+
+    constructor() {
+        this.userID = "test";
+        this.behaviorVisible = true;
+
+        let taskOnCanvas = this.makeCanvas(540);
+        let taskOffCanvas = this.makeCanvas(540);
+        let behaviorOnCanvas = this.makeCanvas(540);
+        let behaviorOffCanvas = this.makeCanvas(540);
+
+        document.getElementById("taskCanvasParent")?.appendChild(taskOnCanvas);
+        document
+            .getElementById("offscreenTaskParent")
+            ?.appendChild(taskOffCanvas);
+        document
+            .getElementById("behaviorCanvasParent")
+            ?.appendChild(behaviorOnCanvas);
+        document
+            .getElementById("offscreenBehaviorParent")
+            ?.appendChild(behaviorOffCanvas);
+
+        this.taskController = new TSPTaskController(
+            taskOnCanvas,
+            taskOffCanvas,
+            window.requestAnimationFrame,
+            this.onNewSolution,
+            () => this.currentSolution.slice()
+        );
+        this.behaviorController = new TSPBehaviorController(
+            behaviorOnCanvas,
+            behaviorOffCanvas,
+            window.requestAnimationFrame,
+            this.onNewSolution
+        );
+
+        // init
+        this.onNewSolution(
+            "random start",
+            this.taskController.model.getRandomSolution()
+        );
+    }
+    sendLog(type: String, info: {}): void {
+        throw new Error("Method not implemented.");
+    }
+    logTick(): void {
+        throw new Error("Method not implemented.");
+    }
+    chooseGame(): void {
+        throw new Error("Method not implemented.");
+    }
+    requestLastSolution(): void {
+        throw new Error("Method not implemented.");
+    }
+    requestBestSolution(): void {
+        throw new Error("Method not implemented.");
+    }
+    onNewSolution = (type: String, solution: any) => {
+        console.log("onNewSolution called with:", type, solution);
+        this.previousSolution = this.currentSolution.slice();
+        this.currentSolution = solution.slice();
+        // send solution to task model for scoring
+        let score = this.taskController.model.scoreSolution(solution);
+        // solution sent w grade to behavior model for behavior scoring
+        let behavior = this.behaviorController.model.evaluateSolution(
+            this.taskController.model.problem,
+            solution,
+            score
+        );
+        this.taskController.view.draw();
+        this.behaviorController.view.draw();
+        // solution, problem, solutionScore, isAnElite, behaviorBin, behaviorScore, type sent to log
+        // check if solution is the best one
+    };
+
+    makeCanvas(size: number) {
+        let canvas = document.createElement("canvas");
+        canvas.width = size;
+        canvas.height = size;
+        return canvas;
+    }
+}
---
+++ manager/Manager.ts
@@ -0,0 +1,23 @@
+import BehaviorController from "../controller/BehaviorController.js";
+import TaskController from "../controller/TaskController.js";
+import Solution from "../interfaces/Solution.js";
+
+export default interface Manager {
+    userID: String;
+    behaviorVisible: boolean;
+    bestSolution: Solution;
+    previousSolution: Solution;
+    currentSolution: Solution;
+    bestScore: number;
+
+    taskController: TaskController;
+    behaviorController: BehaviorController;
+
+    sendLog(type: String, info: {}): void;
+    logTick(): void;
+    chooseGame(): void;
+    requestLastSolution(): void;
+    requestBestSolution(): void;
+
+    onNewSolution(type: String, solution: Solution): void;
+}
